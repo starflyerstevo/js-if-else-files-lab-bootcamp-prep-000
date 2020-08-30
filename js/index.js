d4f96da34b2ace2a41dd92e8aa859dd4764caf8e
Stephen Ramsden <starflyerstevo@gmail.com>
2020-08-30 19:42:28 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,9 +3,7 @@ var height = "70"
 if (name === "Susan")
 {
   console.log("The name is correct")
-} else {
-  console.log("Expected the name to be Susan")
-}
+} 
 if(height === "70")
 {
   console.log("This height is correct")}
